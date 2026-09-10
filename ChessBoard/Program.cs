7e0f5e8d4642f1216d1b65fe8cf2e1a7eab944fc
espi1968 <esa.mourujarvi@telia.com>
2026-09-10 19:56:43 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // haha denna kämpade jag med för jag hade bara ? tecken.
+
+            Console.Write("How many rows?");
+            int rows = Int32.Parse(Console.ReadLine());
+
+            Console.Write("How many columns?");
+            int columns = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            char symbol1 = '◼';
+            char symbol2 = '◻';
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if ((row + col) % 2 == 0)
+                    {
+                        Console.Write(symbol1);
+                    }
+                    else
+                    {
+                        Console.Write(symbol2);
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+               
+        }
+    }
+}
